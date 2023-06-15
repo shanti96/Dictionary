7b878933df1a3316c60bec96f6830e1eefc74e91
shanti96 <106430039+shanti96@users.noreply.github.com>
2023-06-15 15:10:17 +0530

    Add files via upload

---
+++ example.py
@@ -0,0 +1,16 @@
+# dic={"num":23,"sel":40,"tuple":(20,3,12,11),"list":[1,3,5,0,2]}
+# print(dic["sel"]+dic['num']) 
+
+"""dil={"a":{1:'hello',2:'hii',3:'navgurukul'},'b':{4:'wel',5:'come'}}
+dil['b'][6]="to" 
+print(dil["b"])
+
+dir={'nil':1.9,'bil':2}  
+dir['mil']=3 
+print(dir) """
+
+class nav:
+    var_streght=100
+    def resi():
+        print("called res function")
+    resi()  
